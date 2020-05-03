27e629833acdb380363cdb40cd7223a531c3aaca
chonghaozhang1998 <chonghaozhang1998@gmail.com>
2020-05-04 03:01:30 +0800

    completed LeapYear of lab1

--- lab1/LeapYear.java
+++ lab1/LeapYear.java
@@ -3,6 +3,18 @@
  */
 public class LeapYear {
 
+    /**
+     * 
+     * @param year
+     */
+    public static boolean isLeapYear(int year) {
+        if(year % 400 == 0 || (year % 4 == 0 && year % 100 != 0)) {
+            return true;
+        } else{
+            return false;
+        }
+    }
+
     /** Calls isLeapYear to print correct statement.
      *  @param  year to be analyzed
      */
