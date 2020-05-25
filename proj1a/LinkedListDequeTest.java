ca01a793f06aaa9c8260dda953d29d14ef65caec
chonghaozhang1998 <chonghaozhang1998@gmail.com>
2020-05-25 15:50:07 +0800

    changed code style(delete tab)

--- proj1a/LinkedListDequeTest.java
+++ proj1a/LinkedListDequeTest.java
@@ -39,31 +39,23 @@ public class LinkedListDequeTest {
      */
     public static void addIsEmptySizeTest() {
         System.out.println("Running add/isEmpty/Size test.");
-        System.out.println("Make sure to uncomment the " +
-                "lines below (and delete this print statement).");
-		/*
-		LinkedListDeque<String> lld1 = new LinkedListDeque<String>();
-
-		boolean passed = checkEmpty(true, lld1.isEmpty());
-
-		lld1.addFirst("front");
-		
-		// The && operator is the same as "and" in Python.
-		// It's a binary operator that returns true if both arguments true, and false otherwise.
-		passed = checkSize(1, lld1.size()) && passed;
-		passed = checkEmpty(false, lld1.isEmpty()) && passed;
-
-		lld1.addLast("middle");
-		passed = checkSize(2, lld1.size()) && passed;
-
-		lld1.addLast("back");
-		passed = checkSize(3, lld1.size()) && passed;
-
-		System.out.println("Printing out deque: ");
-		lld1.printDeque();
-
-		printTestStatus(passed);
-		*/
+        System.out.println("Make sure to uncomment the "
+                + "lines below (and delete this print statement).");
+/*LinkedListDeque<String> lld1 = new LinkedListDeque<String>();
+boolean passed = checkEmpty(true, lld1.isEmpty());
+lld1.addFirst("front");
+// The && operator is the same as "and" in Python.
+// It's a binary operator that returns true if both arguments true, and false otherwise.
+passed = checkSize(1, lld1.size()) && passed;
+passed = checkEmpty(false, lld1.isEmpty()) && passed;
+lld1.addLast("middle");
+passed = checkSize(2, lld1.size()) && passed;
+lld1.addLast("back");
+passed = checkSize(3, lld1.size()) && passed;
+System.out.println("Printing out deque: ");
+lld1.printDeque();
+printTestStatus(passed);
+*/
     }
 
     /**
@@ -72,30 +64,25 @@ public class LinkedListDequeTest {
     public static void addRemoveTest() {
 
         System.out.println("Running add/remove test.");
-        System.out.println("Make sure to uncomment the" +
-                " lines below (and delete this print statement).");
-		/*
-		LinkedListDeque<Integer> lld1 = new LinkedListDeque<Integer>();
-		// should be empty
-		boolean passed = checkEmpty(true, lld1.isEmpty());
-
-		lld1.addFirst(10);
-		// should not be empty
-		passed = checkEmpty(false, lld1.isEmpty()) && passed;
-
-		lld1.removeFirst();
-		// should be empty
-		passed = checkEmpty(true, lld1.isEmpty()) && passed;
-
-		printTestStatus(passed);
-		*/
+        System.out.println("Make sure to uncomment the"
+                + " lines below (and delete this print statement).");
+/*
+LinkedListDeque<Integer> lld1 = new LinkedListDeque<Integer>();
+// should be empty
+boolean passed = checkEmpty(true, lld1.isEmpty());
+lld1.addFirst(10);
+// should not be empty
+passed = checkEmpty(false, lld1.isEmpty()) && passed;
+lld1.removeFirst();
+// should be empty
+passed = checkEmpty(true, lld1.isEmpty()) && passed;
+printTestStatus(passed);
+*/
     }
 
     public static void main(String[] args) {
         System.out.println("Running tests.\n");
         addIsEmptySizeTest();
         addRemoveTest();
-
     }
-
-} 
+}
