cfa09f95e05dd3288b0c3b2541003f442fec4386
chonghaozhang1998 <chonghaozhang1998@gmail.com>
2020-09-09 02:51:25 +0800

    第一次完成hw2

--- hw2/hw2/Percolation.java
+++ hw2/hw2/Percolation.java
@@ -1,7 +1,148 @@
 package hw2;
 
+import edu.princeton.cs.algs4.StdRandom;
 import edu.princeton.cs.algs4.WeightedQuickUnionUF;
 
 public class Percolation {
+    private WeightedQuickUnionUF weightedQuickUnionUF;
+    private boolean[][] sites;
+    // represents whether site is open or not
+    // true represents that site is open
+    // false represents that site is not open
+    private int numberOfOpenSites;
 
+    private int N;
+
+    private int currentIndices(int row, int col) {
+        return this.N * row + col;
+    }
+
+    //create N-by-N grid, with all sites initiall
+    public Percolation(int N) {
+        if (N <= 0) {
+            throw new IllegalArgumentException("Wrong intput(N should > 0)");
+        }
+        this.N = N;
+        this.weightedQuickUnionUF = new WeightedQuickUnionUF(N * N);
+        // weightedQuickUnionUF does not contain N * N but contains N * N - 1
+        this.numberOfOpenSites = 0;
+        this.sites = new boolean[N][N];
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                this.sites[i][j] = false;
+            }
+        }
+    }
+
+    //open the site (row, col) if it is not open
+    public void open(int row, int col) {
+        if (row < 0 || row > this.N - 1 || col < 0 || col > this.N - 1) {
+            throw new IndexOutOfBoundsException("Wrong input(row and col should be integers between 0 and N - 1)");
+        }
+        if (!sites[row][col]) {
+            this.sites[row][col] = true;
+            this.numberOfOpenSites += 1;
+            int currentIndices = currentIndices(row, col);
+            int aboveIndices = currentIndices - this.N;
+            int belowIndices = currentIndices + this.N;
+            int leftIndices = currentIndices - 1;
+            int rightIndices = currentIndices + 1;
+            boolean above = true;
+            boolean below = true;
+            boolean left = true;
+            boolean right = true;
+            if (row == 0) {
+                above = false;
+            }
+            if (row == this.N - 1) {
+                below = false;
+            }
+            if (col == 0) {
+                left = false;
+            }
+            if (col == this.N - 1) {
+                right = false;
+            }
+            if (above == true && sites[row - 1][col]) {
+                weightedQuickUnionUF.union(currentIndices, aboveIndices);
+            }
+            if (below == true && sites[row + 1][col]) {
+                weightedQuickUnionUF.union(currentIndices, belowIndices);
+            }
+            if (left == true && sites[row][col - 1]) {
+                weightedQuickUnionUF.union(currentIndices, leftIndices);
+            }
+            if (right == true && sites[row][col + 1]) {
+                weightedQuickUnionUF.union(currentIndices, rightIndices);
+            }
+        }
+    }
+
+    //is the site (row, col) open?
+    public boolean isOpen(int row, int col) {
+        if (row < 0 || row > this.N - 1 || col < 0 || col > this.N - 1) {
+            throw new IndexOutOfBoundsException("Wrong input(row and col should be integers between 0 and N - 1)");
+        }
+        return this.sites[row][col];
+    }
+
+    //is the site (row, col) full?
+    public boolean isFull(int row, int col) {
+        if (row < 0 || row > this.N - 1 || col < 0 || col > this.N - 1) {
+            throw new IndexOutOfBoundsException("Wrong input(row and col should be integers between 0 and N - 1)");
+        }
+        boolean res = false;
+        if (!this.isOpen(row, col)) {
+            return res;
+        }
+        int currentIndices = currentIndices(row, col);
+        for (int i = 0; i < this.N; i++) {
+            if (weightedQuickUnionUF.connected(i, currentIndices)) {
+                res = true;
+                break;
+            }
+        }
+        return res;
+    }
+
+    //number of open sites
+    public int numberOfOpenSites() {
+        return this.numberOfOpenSites;
+    }
+
+    //does the system percolate?
+    public boolean percolates() {
+        boolean res = false;
+        for (int top = 0; top < this.N; top++) {
+            int start = currentIndices(this.N - 1, 0);
+            int end = currentIndices(this.N - 1, this.N - 1);
+            for (int bottom = start; bottom <= end; bottom++) {
+                if (weightedQuickUnionUF.connected(top, bottom)) {
+                    res = true;
+                    break;
+                }
+            }
+        }
+        return res;
+    }
+
+    //use for unit testing (not required)
+    public static void main(String[] args) {
+        Percolation percolation = new Percolation(5);
+        percolation.open(0, 0);
+        percolation.open(4, 0);
+        percolation.open(4, 4);
+        percolation.open(0, 4);
+        System.out.println(percolation.isOpen(0, 0));
+        System.out.println(percolation.isOpen(0, 4));
+        System.out.println(percolation.isOpen(4, 4));
+        System.out.println(percolation.isOpen(4, 0));
+        System.out.println(percolation.isFull(0, 0));
+        System.out.println(percolation.isFull(0, 4));
+        System.out.println(percolation.isFull(4, 4));
+        System.out.println(percolation.isFull(4, 0));
+        System.out.println(percolation.isFull(0, 1));
+        System.out.println(percolation.percolates());
+        System.out.println(percolation.numberOfOpenSites());
+    }
 }
--- hw2/hw2/PercolationStats.java
+++ hw2/hw2/PercolationStats.java
@@ -1,5 +1,63 @@
 package hw2;
 
+import edu.princeton.cs.introcs.StdRandom;
+import edu.princeton.cs.introcs.StdStats;
+
+
 public class PercolationStats {
+    double[] results; // record the fraction of sites that are opened when the system percolates
+
+    // perform T independent experiments on an N-by-N grid
+    public PercolationStats(int N, int T, PercolationFactory pf) {
+        if (N <= 0 || T <= 0) {
+            throw new IllegalArgumentException("Wrong input(N and T should larger than zero)");
+        }
+        results = new double[T];
+        for (int i = 0; i < T; i++) {
+            Percolation percolation = pf.make(N);
+            while (!percolation.percolates()) {
+                int row = StdRandom.uniform(0, N);
+                int col = StdRandom.uniform(0, N);
+                // StdRandom.uniform(int a, int b) uniformly generate integers between a(inclusive) and b(exclusive)
+                percolation.open(row, col);
+            }
+            double fractionOfOpenToBlocked = percolation.numberOfOpenSites() / (N * N);
+            results[i] = fractionOfOpenToBlocked;
+        }
+    }
+
+    //sample mean of percolation threshold
+    public double mean() {
+        return StdStats.mean(results);
+//        double sum = 0;
+//        for (double res : results) {
+//            sum += res;
+//        }
+//        return sum / results.length;
+    }
+
+    //sample standard deviation of percolation threshold
+    public double stddev() {
+        return StdStats.stddev(results);
+//        if (results.length <= 1) {
+//            throw new IllegalArgumentException("Times(T) should be larger than 1");
+//        }
+//        double variance = 0;
+//        double mean = this.mean();
+//        for (int i = 0; i < results.length; i++) {
+//            variance += Math.pow (results[i] - mean, 2);
+//        }
+//        return Math.pow(variance / (results.length - 1), 0.5);
+    }
+
+    //low endpoint of 95% confidence interval
+    public double confidenceLow() {
+        return this.mean() - 1.96 * this.stddev() / Math.pow(results.length, 0.5);
+    }
+
+    //high endpoin of 95% confidence interval
+    public double confidenceHigh() {
+        return this.mean() + 1.96 * this.stddev() / Math.pow(results.length, 0.5);
+    }
 
 }
